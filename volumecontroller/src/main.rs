a463f783c5d2473e21e6897d0996b45364982960
Boomkop3 <sjors.de.bruijn@gmail.com>
2021-03-07 12:54:43 +0100

    Rust http sender now works with a command line parameter

--- volumecontroller/src/main.rs
+++ volumecontroller/src/main.rs
@@ -1,19 +1,30 @@
+use std::{env};
 use isahc::HttpClient;
 
 fn main() {
-    change_volume(VolumeDirection::UP).unwrap();
+    let args: Vec<String> = env::args().collect();
+    if  args.len() != 2 {
+        print_help();
+        return;
+    }
+    match args[1].as_str() {
+        "UP" => change_volume(VolumeDirection::UP).unwrap(), 
+        "DOWN" => change_volume(VolumeDirection::DOWN).unwrap(), 
+        _ => print_help()
+    }
 }
 
-fn change_volume(_direction: VolumeDirection) -> Result<(), ()> {
+fn change_volume(direction: VolumeDirection) -> Result<(), ()> {
     let client = HttpClient::new();
     if client.is_err(){
         return Err(());
     }
     let client = client.unwrap();
-    let volume_char = ">";
-
-    
-
+    let volume_char;
+    match direction {
+        VolumeDirection::UP => volume_char = ">",
+        VolumeDirection::DOWN => volume_char = "<"
+    }
     let client = client.post(
         "http://192.168.0.150/MainZone/index.put.asp", 
         format!("cmd0=PutMasterVolumeBtn/{}", volume_char)
@@ -24,4 +35,13 @@ fn change_volume(_direction: VolumeDirection) -> Result<(), ()> {
 
 enum VolumeDirection {
     UP, DOWN
+}
+
+fn print_help(){
+    println!("");
+    println!("Simple Volume Command Sending Thingy");
+    println!("Compatible with DENON AVR 1912");
+    println!("");
+    println!("usage: \r\n\tvolumecontroller.exe UP/DOWN [hostname]");
+    println!("example: \r\n\tvolumecontroller.exe UP 192.168.0.100");
 }
